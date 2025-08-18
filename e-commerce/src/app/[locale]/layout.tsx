b81a37ad0a8fa6e966a4f18ee91ae62d9ce85f18
alper sonat <75118270+alpEr0640@users.noreply.github.com>
2025-08-18 14:04:29 +0300

    Update layout.tsx

--- e-commerce/src/app/[locale]/layout.tsx
+++ e-commerce/src/app/[locale]/layout.tsx
@@ -27,7 +27,7 @@ export default async function LocaleLayout({
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  const { locale } = await params;
+  const { locale } =  params;
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
